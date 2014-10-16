e03ce0ad0909b5cd45c624bd840337d2f4d1d4a5
davey <davey@dpwoert.com>
2014-10-16 22:47:59 +0200

    timeline design

--- packages/deepspace-part-timeline/timeline.js
+++ packages/deepspace-part-timeline/timeline.js
@@ -13,11 +13,11 @@ var calculateScales = function(){
     //calculate extremes
     min = d3.min(data, function(d){ return d.length; });
     max = d3.max(data, function(d){ return d.length; });
-    var groups = data.length;
+    var groups = data.length - 1;
 
     //scales
     scales.x = d3.scale.linear().domain([0, groups]).range([left, right]);
-    scales.y = d3.scale.linear().domain([0, max]).range([0, height]);
+    scales.y = d3.scale.linear().domain([max, 0]).range([0, height]);
 
 };
 
@@ -28,11 +28,32 @@ var render = function(){
         .x(function(d,i) { return scales.x(i); })
         .y(function(d,i) { return scales.y( d.length ); })
         .interpolate('basis');
+        // .interpolate('bundle');
 
     //save line
     Session.set('path', line(data));
 
 };
+
+var circles = function(){
+
+    var circles = [];
+    //add circles for each row
+    _.each(data, function(row, key){
+
+        if(key % 6 === 0){
+
+            circles.push({
+                x: scales.x( key ),
+                y: scales.y( row.length )
+            });
+
+        }
+    });
+
+    Session.set('pathCircles', circles);
+
+}
 
 Template.timeline.rendered = function(){
 
@@ -43,8 +64,15 @@ Template.timeline.rendered = function(){
     calculateScales();
     render();
 
+    //add circles to timeline
+    circles();
+
 };
 
 Template.timeline.path = function(){
     return Session.get('path');
+};
+
+Template.timeline.circles = function(){
+    return Session.get('pathCircles');
 };
